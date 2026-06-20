52717ba24f27e83ef8029732550dbdcc5599a32f
ZAIN <itszainahmed96800@gmail.com>
2026-06-20 19:42:45 +0530

    Implement CORS and problem validation models
    Added CORS middleware and problem validation models.

--- main_api.py
+++ main_api.py
@@ -5,6 +5,71 @@ from datetime import date
 from database import get_connection
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Problem(BaseModel):
+
+    name: str
+    topic: str
+    difficulty: str
+
+    @field_validator("difficulty")
+    @classmethod
+    def validate_difficulty(cls, value):
+
+        value = value.strip().capitalize()
+
+        allowed = [
+            "Easy",
+            "Medium",
+            "Hard"
+        ]
+
+        if value not in allowed:
+            raise ValueError(
+                "Difficulty must be Easy, Medium or Hard"
+            )
+
+        return value
+
+
+class UpdateProblem(BaseModel):
+
+    topic: str
+    difficulty: str
+
+    @field_validator("difficulty")
+    @classmethod
+    def validate_difficulty(cls, value):
+
+        value = value.strip().capitalize()
+
+        allowed = [
+            "Easy",
+            "Medium",
+            "Hard"
+        ]
+
+        if value not in allowed:
+            raise ValueError(
+                "Difficulty must be Easy, Medium or Hard"
+            )
+
+        return value
+
+
+class ProblemResponse(BaseModel):
+    id: int
+    name: str
+    topic: str
+    difficulty: str
+    solved_date: date
 
 
 @app.get("/")
